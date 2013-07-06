e8e61b172eb64283f3cfbf0be4cdcb338582ba78
david <teacher3010@163.com>
2013-07-06 21:24:55 +0800

    add equals & hashCode to User.java

--- src/main/java/com/itreply/crud/entity/User.java
+++ src/main/java/com/itreply/crud/entity/User.java
@@ -3,6 +3,9 @@ package com.itreply.crud.entity;
 import java.io.Serializable;
 
 public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
 	private String userName;
 	private String userPwd;
 	private String userRole;
@@ -24,5 +27,43 @@ public class User implements Serializable {
 	public void setUserRole(String userRole) {
 		this.userRole = userRole;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((userName == null) ? 0 : userName.hashCode());
+		result = prime * result + ((userPwd == null) ? 0 : userPwd.hashCode());
+		result = prime * result
+				+ ((userRole == null) ? 0 : userRole.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		if (userPwd == null) {
+			if (other.userPwd != null)
+				return false;
+		} else if (!userPwd.equals(other.userPwd))
+			return false;
+		if (userRole == null) {
+			if (other.userRole != null)
+				return false;
+		} else if (!userRole.equals(other.userRole))
+			return false;
+		return true;
+	}
+	
 	
 }
