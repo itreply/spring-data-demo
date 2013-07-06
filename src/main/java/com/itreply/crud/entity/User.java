242cd09c02ae0bb5ffa676d4c6ea1b5fa8a2807d
david <teacher3010@163.com>
2013-07-06 21:18:20 +0800

    add code format.xml

--- src/main/java/com/itreply/crud/entity/User.java
+++ src/main/java/com/itreply/crud/entity/User.java
@@ -1,7 +1,28 @@
 package com.itreply.crud.entity;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
 	private String userName;
 	private String userPwd;
 	private String userRole;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	public String getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+	
 }
